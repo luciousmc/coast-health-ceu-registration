a7c904f7f7d26c93ee81eec6bb9e8c71436825bc
Marlon <luciousmc@icloud.com>
2020-08-02 13:27:28 -0700

    express server running

--- server/index.js
+++ server/index.js
@@ -6,6 +6,10 @@ const server = express();
 
 server.use(staticMiddleware);
 
+server.get("/api", (req, res, next) => {
+  res.json({ message: "Server is working" });
+});
+
 server.listen(process.env.SERVER_PORT, () => {
-  console.log("Server is lisening on port" + process.env.SERVER_PORT);
+  console.log("Server is lisening on port " + process.env.SERVER_PORT);
 });
